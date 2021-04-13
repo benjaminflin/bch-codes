252a52fcee2b50c5e752dd0c23d6ba49b5c33a28
Benjamin Flin <brf2117@columbia.edu>
2021-04-13 03:37:34 -0400

    some implementation done

--- bch.py
+++ bch.py
@@ -1,4 +1,8 @@
 import random
+import functools
+from sympy import *
+from sympy.abc import x, alpha
+from sympy.polys.galoistools import gf_rem, gf_compose
 
 class Polynomial:
     def __init__(self, coeff = []):
@@ -40,11 +44,13 @@ class Polynomial:
             return self.evaluate_polynomial(value)
     
     def evaluate_polynomial(self, poly):
-        result = Polynomial([])
-        for i in range(self.deg() + 1):
-            if(self.coeff[i] == 1):
-                result += poly**(self.deg() - i)
-        return result
+        # result = Polynomial([])
+        # for i in range(self.deg() + 1):
+        #     if(self.coeff[i] == 1):
+        #         result += poly**(self.deg() - i)
+        # return result
+
+        return Polynomial(gf_compose(ZZ.map(self.coeff), ZZ.map(poly.coeff), 2, ZZ))
         
 
     def __add__(self, other):
@@ -70,53 +76,71 @@ class Polynomial:
         for i in range(exp - 1):
             result = result * self
         return result
+    
+    def remainder(self, poly):
+        return Polynomial(gf_rem(ZZ.map(self.coeff), ZZ.map(poly.coeff), 2, ZZ))
 
 
-# minimum distance
-distance = 3 
+
+# 1. Create a list (list1) of x^i mod generator_poly for 0 <= i < 256 (really alphas)
+# 2. Another, parallel list (list2) of each of the above polynomials with coefficients 
+#    listwise interpreted as a binary integer (x^2 + 1 = 101 = 5)
+# 3. In order to calculate generator_poly(alpha^i), calculate generator_poly(x^i), 
+#    and then for each term x^j, look up and xor list2[j % 255]. (adding in F_2[x])
+# 4. Interpret final result as polynomial.
+
 
 # (n,k) = (255, 239) generator polynomial 
 # Citation: https://link.springer.com/content/pdf/bbm%3A978-1-4899-2174-1%2F1.pdf
 # The octal notation was converted to binary
-generator_poly = Polynomial([ int(x) for x in list("10110111101100011")])
-alpha = Polynomial([ int(x) for x in "101110111" ]) 
+generator_poly = Poly.from_list([ int(i) for i in list("10110111101100011")], gens=[x], domain=GF(2)) 
 
-# 239 = bit message to send
-msg = 239 * [0]
-for i in range(239):
-    msg[i] = random.randint(0, 1) 
-msg_poly = Polynomial(msg) 
+print(generator_poly.all_coeffs())
+basis_poly = Poly.from_list([ int(i) for i in list("100011101")], gens=[x], domain=GF(2)) # NOT 101110111
 
+powers_of_alpha = [ Poly(x ** i, gens=[x], domain=GF(2)).rem(basis_poly) for i in range(256) ]
+list2 = [ p.all_coeffs() for p in powers_of_alpha ]
 
+def eval_at_alpha_pow(poly, i):
+    result = [0, 0, 0, 0, 0, 0, 0, 0]
+    poly = compose(poly, x**i)
+    for j in poly.all_coeffs():
+        for i in range(len(list2[j % 255])):
+            result[i] ^= list2[j % 255][i]  
+    return Poly.from_list(result, gens=[x], domain=GF(2))
 
-if __name__ == '__main__':  
-
-    err_rate = float(input("Enter desired error rate: "))
-
-    print("--- Message to be encoded ---")
-    print(msg_poly.pretty())
-
-    # generate encoded_msg 
-    encoded_msg = generator_poly * msg_poly
-
-    print("--- Encoded message ---")
-    print(encoded_msg.pretty())
-    # randomly permute encoded message 
-    for i in range(len(encoded_msg.coeff)):
-        if random.random() < err_rate:
-            encoded_msg.coeff[i] = random.randint(0, 1) 
-
-    print("--- Permuted message ---")
-    print(encoded_msg.pretty())
-
-    poly1 = Polynomial([1,0,1,1])
-    poly2 = Polynomial([1,1,0])
-    print(poly1)
-    print(poly2)
-    print(poly1*poly2)
-    print(generator_poly.evaluate(alpha).pretty())
+print(eval_at_alpha_pow(generator_poly, 2))
 
 
 
 
 
+
+
+
+# if __name__ == '__main__':  
+
+#     err_rate = float(input("Enter desired error rate: "))
+
+#     print("--- Message to be encoded ---")
+#     print(msg_poly.pretty())
+
+#     # generate encoded_msg 
+#     encoded_msg = generator_poly * msg_poly
+
+#     print("--- Encoded message ---")
+#     print(encoded_msg.pretty())
+#     # randomly permute encoded message 
+#     for i in range(len(encoded_msg.coeff)):
+#         if random.random() < err_rate:
+#             encoded_msg.coeff[i] = random.randint(0, 1) 
+
+#     print("--- Permuted message ---")
+#     print(encoded_msg.pretty())
+
+#     poly1 = Polynomial([1,0,1,1])
+#     poly2 = Polynomial([1,1,0])
+#     print(poly1)
+#     print(poly2)
+#     print(poly1*poly2)
+#     print(generator_poly.evaluate(alpha).pretty())
